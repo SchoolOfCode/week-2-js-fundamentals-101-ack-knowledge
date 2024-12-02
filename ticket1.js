2bafe9e7de0a3afe30ed8eaf5327023bee1d33ad
Adam Hemmings <38017169+Makenshii@users.noreply.github.com>
2024-12-02 15:50:48 +0000

    added convertGBPToCurrency function

--- ticket1.js
+++ ticket1.js
@@ -4,12 +4,58 @@
 //  that takes in a specified amount in £,
 //  and converts it to €
 // using a given exchange rate. The function should return the converted amount.
+
+//MVP 1
+
 function convertGBPtoEUR(pounds) {
+
   let euros = pounds * 1.21;
+
   return euros;
 }
 
 // 2. 60% said understanding what their spending budget in £ is worth in $
+
+//Write another function convertGBPtoUSD that takes in a specified amount in £, ✅
+//and converts it to $ using a given exchange rate(1.27). ✅
+//It should behave in a very similar way to the previous function.✅
+
+//MVP 2
+
+function convertGBPtoUSD(pounds) {
+
+  let dollars = pounds * 1.27;
+
+  return dollars;
+}
+
+
+// Write a function `convertGBPToCurrency` 
+// which takes in both a currency code (`"EUR"` or `"USD"`) 
+// and an amount of £ to convert. EUR 5
+// It should, based on which code has been entered, use one of your previously written functions to return the correct conversion. 
+// If an unknown code is entered, it should return `null`.
+
+// user inputs code (EUR or USD)
+// define varibles code EUR -> exhcange rate
+
+//let EUR = convertGBPtoEUR(pounds); 
+//let USD = convertGBPtoUSD(pounds);
+
+
+// exchange rate will be multiplied by the user amount and return GBP conversion
+
+function convertGBPToCurrency(code, value) {
+
+  if (code === "EUR") {
+    thing = convertGBPtoEUR(value);
+  }     
+  else if  (code === "USD"){
+    thing = convertGBPtoUSD(value);
+  }
+  else return null
+  return thing;
+} 
 
 // 3. 10% said being able to book transport through an app
 
@@ -17,13 +63,7 @@ function convertGBPtoEUR(pounds) {
 
 // 5. 2% said getting alerts about deals on hotels they've previously seen
 
-//MVP 2
 
-//Write another function convertGBPtoUSD that takes in a specified amount in £, ✅
-//and converts it to $ using a given exchange rate(1.27). ✅
-//It should behave in a very similar way to the previous function.✅
 
-function convertGBPtoUSD(pounds) {
- let dollars= pounds * 1.27;
- return dollars;
-}
+
+
