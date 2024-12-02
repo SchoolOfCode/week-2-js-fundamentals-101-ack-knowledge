0e61e207bf50006e8e2d4c8ba0270c88522f7ccb
Adam Hemmings <38017169+Makenshii@users.noreply.github.com>
2024-12-02 19:27:41 +0000

    added function to convert fahrenheit to celsius

---
+++ ticket2.js
@@ -0,0 +1,18 @@
+// Develop a JavaScript function displayTemperatureInCelsius 
+// that takes in a numerical Fahrenheit temperature reading 
+// converts it to Celsius, 
+// returning the number representing the Celsius value.
+
+
+// function for conversion fahrenheit to celsius
+function convertFahrenheitToCelsius(fahrenheit){
+
+    return (fahrenheit - 32) * 5 / 9;
+
+}
+
+
+
+
+
+
