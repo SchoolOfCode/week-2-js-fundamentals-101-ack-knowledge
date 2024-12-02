02fa812508282c6d7e61ef170d489ce165c6edc2
Kim Lida <88290393+Kinukeo@users.noreply.github.com>
2024-12-02 13:32:58 +0000

    created convertGBPtoEUR

---
+++ ticket1.js
@@ -0,0 +1,18 @@
+// 1. 80% said understanding what their spending budget in £ is worth in €
+
+// Write a JavaScript function `convertGBPtoEUR`
+//  that takes in a specified amount in £,
+//  and converts it to €
+// using a given exchange rate. The function should return the converted amount.
+function convertGBPtoEUR(pounds) {
+  let euros = pounds * 1.21;
+  return euros;
+}
+
+// 2. 60% said understanding what their spending budget in £ is worth in $
+
+// 3. 10% said being able to book transport through an app
+
+// 4. 5% said being able to book hotels through an app
+
+// 5. 2% said getting alerts about deals on hotels they've previously seen
