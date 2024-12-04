ab68fb120e9a3ba8bfc6a7b15e515caa81d24db0
Adam Hemmings <38017169+Makenshii@users.noreply.github.com>
2024-12-04 14:38:40 +0000

    "solved ticket 3"

---
+++ ticket3.js
@@ -0,0 +1,14 @@
+// 1. 70% indicated that discounts would encourage them to buy more.
+// 2. 20% were interested in a loyalty program that rewards regular customers.
+// 3. 10% suggested more author events and book signings.
+
+// Develop a JavaScript function `applyDiscount` 
+// calculates the final price after applying a 10% discount on single purchases over £20. 
+// This function should help increase the average purchase value by offering an immediate incentive.
+
+function applyDiscount(sale) {
+
+    return sale;
+
+
+}
